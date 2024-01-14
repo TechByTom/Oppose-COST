53a2774a9175528b9343058e528736602abcfe6f
root <root@o-cost-debian-s-2vcpu-4gb-amd-nyc3-01>
2024-01-14 06:34:35 +0000

    Prettied up the admin interface, and started work on building applications when the button is clicked

--- server.go
+++ server.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-    "crypto/rand"
+    "bufio"
     "encoding/json"
+    "crypto/rand"
     "fmt"
-    "html/template"
     "io"
     "log"
     "net/http"
     "os"
-    "os/exec"
-    "path/filepath"
+    "html/template"
 )
 
 // ClientInfo represents the information about a client application
@@ -19,7 +18,6 @@ type ClientInfo struct {
     Hostname string // To be filled when provided by the client
 }
 
-// logClientInfo logs the client information to a file
 func logClientInfo(filename string, info ClientInfo) error {
     file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
@@ -39,21 +37,22 @@ func logClientInfo(filename string, info ClientInfo) error {
     return nil
 }
 
-// generateUUID generates a new UUID
 func generateUUID() (string, error) {
     uuid := make([]byte, 16)
     n, err := io.ReadFull(rand.Reader, uuid)
     if n != len(uuid) || err != nil {
         return "", err
     }
+    
+    // variant bits; see section 4.1.1
+    uuid[8] = uuid[8]&^0xc0 | 0x80
 
-    uuid[8] = uuid[8]&^0xc0 | 0x80 // variant bits
-    uuid[6] = uuid[6]&^0xf0 | 0x40 // version 4
+    // version 4 (pseudo-random); see section 4.1.3
+    uuid[6] = uuid[6]&^0xf0 | 0x40
 
     return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
 
-// handleAdminRequests serves the admin.html page
 func handleAdminRequests(w http.ResponseWriter, r *http.Request) {
     tmpl, err := template.ParseFiles("admin.html")
     if err != nil {
@@ -63,87 +62,63 @@ func handleAdminRequests(w http.ResponseWriter, r *http.Request) {
     tmpl.Execute(w, nil)
 }
 
-// handleClientRequests handles requests from client applications
 func handleClientRequests(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello, Client Application!")
 }
 
-// handleBuildRequest handles build requests for different OS types
 func handleBuildRequest(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "POST" {
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-        return
-    }
-
-    r.ParseForm()
-    systemType := r.FormValue("system")
-
-    cwd, err := os.Getwd()
-    if err != nil {
-        http.Error(w, "Internal Server Error: unable to get current directory", http.StatusInternalServerError)
-        return
-    }
-
-    outputDir := filepath.Join(cwd, "output")
-    if err := os.MkdirAll(outputDir, 0755); err != nil {
-        http.Error(w, "Internal Server Error: unable to create output directory", http.StatusInternalServerError)
-        return
-    }
-
-    clientID, err := generateUUID()
-    if err != nil {
-        http.Error(w, "Internal Server Error: unable to generate UUID", http.StatusInternalServerError)
-        return
-    }
-
-    filename := fmt.Sprintf("%s-%s", systemType, clientID)
-    switch systemType {
-    case "windows":
-        filename += ".exe"
-    case "linux", "macos":
-        // No extension for Linux and macOS
-    default:
-        http.Error(w, "Invalid system type", http.StatusBadRequest)
-        return
-    }
-    outputFile := filepath.Join(outputDir, filename)
-
-    err = compileClientApp(systemType, outputFile)
-    if err != nil {
-        http.Error(w, fmt.Sprintf("Error compiling application: %v", err), http.StatusInternalServerError)
-        return
-    }
-
-    http.ServeFile(w, r, outputFile)
+    // ... existing handleBuildRequest code ...
 }
 
-// compileClientApp compiles the client application for the specified OS
-func compileClientApp(systemType, outputFile string) error {
-    var cmd *exec.Cmd
+func handleClientList(w http.ResponseWriter, r *http.Request) {
+    // Check if client_log.txt exists
+    if _, err := os.Stat("client_log.txt"); os.IsNotExist(err) {
+        // Create the file with default content if it doesn't exist
+        defaultClients := []ClientInfo{
+            {UUID: "placeholder-uuid-1", Hostname: "Placeholder Host 1"},
+            // Add more default clients as needed
+        }
 
-    switch systemType {
-    case "windows":
-        cmd = exec.Command("go", "build", "-o", outputFile+".exe", "./path/to/client/app")
-    case "linux":
-        cmd = exec.Command("go", "build", "-o", outputFile, "./path/to/client/app")
-    case "macos":
-        cmd = exec.Command("go", "build", "-o", outputFile, "./path/to/client/app")
-    default:
-        return fmt.Errorf("unsupported system type")
+        file, err := os.Create("client_log.txt")
+        if err != nil {
+            http.Error(w, "Unable to create file", http.StatusInternalServerError)
+            return
+        }
+        defer file.Close()
+
+        for _, client := range defaultClients {
+            data, _ := json.Marshal(client)
+            file.Write(append(data, '\n'))
+        }
+    }
+    // Open the client_log.txt file
+    file, err := os.Open("client_log.txt")
+    if err != nil {
+        http.Error(w, "File not found", http.StatusInternalServerError)
+        return
+    }
+    defer file.Close()
+
+    var clients []ClientInfo
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        var client ClientInfo
+        if err := json.Unmarshal(scanner.Bytes(), &client); err != nil {
+            continue // Skip lines that can't be unmarshalled
+        }
+        clients = append(clients, client)
     }
 
-    if err := cmd.Run(); err != nil {
-        return err
-    }
-
-    return nil
+    // Return the list of clients as JSON
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(clients)
 }
 
-// main sets up the HTTP server
 func main() {
     adminMux := http.NewServeMux()
     adminMux.HandleFunc("/", handleAdminRequests)
     adminMux.HandleFunc("/build", handleBuildRequest)
+    adminMux.HandleFunc("/clients", handleClientList)
 
     clientMux := http.NewServeMux()
     clientMux.HandleFunc("/", handleClientRequests)
@@ -160,4 +135,3 @@ func main() {
         log.Fatal("Client Server: ", err)
     }
 }
-
